94695546687f43639055403b370c46d99f5beb72
unknown <matteo.delandhuy6@etu-univ.lorraine.fr>
2019-02-14 02:11:42 +0100

    accueil et Formulaire ok, sans clic

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    
+    <app-accueil></app-accueil>
+    <app-form></app-form>
+    
+
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+import Accueil from './components/Accueil.vue';
+import Form from './components/Form.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    appAccueil: Accueil,
+    appForm: Form
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Accueil.vue
@@ -0,0 +1,69 @@
+<template>
+
+<div id="example-1">
+  <h1>GStage</h1>
+
+  <table>
+    <tbody>
+        <tr>
+            <td id="first">{{ FirstOnglet }}</td>
+            <td id="second">{{ SecondOnglet }}</td>
+            <td id="third">{{ ThirdOnglet }}</td>
+        </tr>
+    </tbody>
+  </table>
+
+
+</div>
+
+
+ 
+ 
+
+
+</template>
+
+<script>
+import Vue from 'vue';
+
+
+
+
+export default {
+  name: 'app-accueil',
+  data () {
+    return {
+      FirstOnglet: 'Mon compte',
+      SecondOnglet: 'Soumettre un formulaire',
+      ThirdOnglet: 'Espace communautaire',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+table{
+  width: 100%;
+  height: 400px;;
+  margin-top: 200px; 
+  border-width: 10px;
+}
+
+td{
+ font-size: 50px;
+ color:white;
+ font-style: italic;
+ background-color: black
+
+}
+
+td:hover {
+  background-color: gold;
+}
+
+h1{
+  font-size: 80px;
+}
+</style>
